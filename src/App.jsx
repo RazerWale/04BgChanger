cb299d09f5f339cf24b9e2428db19c0ee9904a38
Anton <anton333mai@gmail.com>
2025-04-07 23:28:37 +0900

    added color buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("orange");
+
+  // const changeColor = (color) => {
+  //   setColor(color);
+  // };
   return (
-    <dispatchEvent className="w-full h-screen duration-200">
+    <div
+      className="w-full h-screen duration-200 bg-gray-800"
+      style={{ backgroundColor: color }}
+    >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button>test</button>
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            green
+          </button>
         </div>
       </div>
-    </dispatchEvent>
+    </div>
   );
 }
 
